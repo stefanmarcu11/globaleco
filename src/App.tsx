91f27bb18f97fad7365e13cf3ad6e05eaeb6bf6a
Stefan Marcu <stefan.marcu@student.unitbv.ro>
2025-01-30 11:42:42 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import logo from './assets/images/GlobalEco-logo.png'
+import './App.css'
+import { DescriptionNature } from './components/descriptionNature'
+import { Keywords } from './components/keys-list'
+import { MultipleCard } from './components/multiple-card'
+import { Block } from './components/footer'
+import { Copyright } from './components/copyright'
+
+function App() {
+
+  return (
+    <>
+      <div className='main'>
+        <div>
+          <img src={logo} className="logo" alt="React logo" />
+        </div>
+        <Keywords></Keywords>
+      </div>
+      <div className='description-section'>
+        <div className="text-section">
+          <DescriptionNature></DescriptionNature>
+        </div>
+      </div>
+      <div className='multiple-card'>
+        <MultipleCard></MultipleCard>
+      </div>
+      <div className='footer'>
+        <Block></Block>
+        <hr className='line'></hr>
+        <Copyright></Copyright>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/footer.tsx
@@ -0,0 +1,49 @@
+import { SiFacebook, SiGithub, SiLinkedin, SiX } from 'react-icons/si'
+import phone from '../assets/images/call.png'
+import address from '../assets/images/location.png'
+import mail from '../assets/images/mail.png'
+
+export const Block = () => {
+  return (
+    <div className="row">
+      <div className="column side">
+        <h2>SC GLOBALECO SRL</h2>
+        <div className='details'>
+          <img src={address} className='details-icon'></img>
+          <p>Comuna Cașin, Strada Andresesti, Nr. 66, Județul Bacău</p>
+        </div>
+        <div className='details'>
+          <img src={phone} className='details-icon'></img>
+          0754286653
+        </div>
+        <div className='details'>
+          <img src={mail} className='details-icon'></img>
+          office@globaleco.org
+        </div>
+      </div>
+
+      <div className="column middle">
+        <div>
+          <h2 className='hover-underline-animation'>Misiunea noastră</h2>
+          <p>
+            Misiunea GLOBALECO este de a contribui activ la un mediu mai curat și mai sănătos printr-o gestionare corectă a deșeurilor și promovarea reciclării. 
+            Compania se angajează să utilizeze metode sustenabile pentru a reduce poluarea și a optimiza procesele de colectare și transport.
+          </p>
+        </div>
+      </div>
+
+      <div className="column side">
+        <h2>Follow us</h2>
+        <div className="column-side-right">
+          <SiFacebook className='facebook-icon'></SiFacebook>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const socialLinks = [
+  { href: 'https://x.com', icon: <SiX /> },
+  { href: 'https://github.com', icon: <SiGithub /> },
+  { href: 'https://www.linkedin.com', icon: <SiLinkedin /> },
+]
